cb732a848bfb7e1dea74a967adb3ed34e0930181
Jeremy Curtis <jeremy.curtis808@gmail.com>
2020-05-06 15:56:48 -0500

    fixing nav styles

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,16 +4,16 @@ import Logo from '../Logo/Logo.js';
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
       return(
-        <div>
+        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', verticalAlign: 'middle', paddingTop: '25px'}}>
           <Logo />
-          <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <nav>
             <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
           </nav>
         </div>
       );
   } else {
     return(
-      <div style={{display: 'flex', justifyContent: 'space-between', verticalAlign: 'middle', paddingTop: '25px'}}>
+      <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', verticalAlign: 'middle', marginTop: '25px'}}>
         <Logo />
         <nav style={{display: 'flex'}}>
           <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
